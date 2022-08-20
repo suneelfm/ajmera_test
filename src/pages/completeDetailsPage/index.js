de29b8eff959719ef446794e40cc0bddf5d48d6f
suneelfm <sfm.20031998@gmail.com>
2022-08-20 15:23:50 +0530

    Updated CompleteDetailsPage

--- src/pages/completeDetailsPage/index.js
+++ src/pages/completeDetailsPage/index.js
@@ -6,12 +6,28 @@ export default function CompleteDetailsPage() {
   const data = useSelector((state) => state.dataReducer);
 
   return (
-    <Grid container>
-      <Grid item xs={3}>
+    <Grid container p={3}>
+      <Grid
+        container
+        justifyContent={"center"}
+        mb={3}
+        fontWeight="bold"
+        fontSize={"calc(2px + 5vmin)"}
+      >
+        Complete Details
+      </Grid>
+      <Grid item xs={12}>
         {Object.keys(data.data).map((key) => (
-          <Grid>
-            <b style={{ margin: "10px" }}>{key}:</b>
-            {data.data[key]}
+          <Grid container>
+            <Grid item xs={2}>
+              <b style={{ margin: "10px" }}>{key}</b>
+            </Grid>
+            <Grid item xs={1}>
+              <b>:</b>
+            </Grid>
+            <Grid item xs={9} sx={{ wordBreak: "break-word" }}>
+              {data.data[key]}
+            </Grid>
           </Grid>
         ))}
       </Grid>
